69111cab18bca61636608ec6bce1317fdd423172
WPBASILAHMAD <basilmedia7@gmail.com>
2022-12-08 17:40:13 +0500

    create file upload icon

--- src/pages/ReplyMessage.jsx
+++ src/pages/ReplyMessage.jsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import Divider from '@mui/material/Divider';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 import IconButton from '@mui/material/IconButton';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import SendIcon from '@mui/icons-material/Send';
-import { TextField } from "@mui/material";
+import { TextField,Tooltip } from "@mui/material";
 import { useState } from "react";
 import Picker from "emoji-picker-react";
-import { common, lime } from "@mui/material/colors";
+import {blue} from "@mui/material/colors";
 
 export default function ReplyMessage() {
     
     //Emoji
   const [inputStr, setInputStr] = useState("");
   const [showPicker, setShowPicker] = useState(false);
+  const [files, setFiles] = useState([]);
 
   const onEmojiClick = (event, emojiObject) => {
     setInputStr((prevInput) => prevInput + emojiObject.emoji);
@@ -22,15 +24,35 @@ export default function ReplyMessage() {
   return (
     <Paper className="reply"
       component="form"
-      sx={{ p: '2px 4px', display: 'flex' ,bgcolor:common}}
-    >
-      <IconButton sx={{ p: '10px' }} aria-label="menu">
-        <EmojiEmotionsIcon onClick={() => setShowPicker((val) => !val)} />
+      sx={{ p: '2px 4px', display: 'flex'}}
+    >  
+
+        {/*File Upload */}
+
+      <input accept="image/*" id="icon-button-file"
+        type="file" style={{ display: 'none' }} value={files}
+        onChange={setFiles} />
+      
+      <Tooltip title="Attach File">
+      <label htmlFor="icon-button-file">
+        <IconButton color="primary" aria-label="upload picture"
+        component="span">
+          <AttachFileIcon />
+        </IconButton>
+      </label>      
+      </Tooltip>
+      
+      {/* Select Emoji */}
+      <Tooltip title="Select Emoji">
+      <IconButton aria-label="menu">
+        <EmojiEmotionsIcon onClick={() => setShowPicker((val) => !val)} sx={{color:blue[800]}} />
       </IconButton>
+      </Tooltip>
+
       {showPicker && (
           <Picker pickerStyle={{ width: "100%" }} onEmojiClick={onEmojiClick} />
         )}
-      <TextField value={inputStr}
+      <TextField type="text" value={inputStr}
           onChange={(e) => setInputStr(e.target.value)} fullWidth id="standard-basic" variant="standard" />
       
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
