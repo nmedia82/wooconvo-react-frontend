0b0d4e21627a9e74c58c56c74a14feb32e35d5ff
WPBASILAHMAD <basilmedia7@gmail.com>
2022-12-13 19:53:13 +0500

    changing in navbar comp

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { alpha, styled } from "@mui/material/styles";
+
 import {
   Chip,
   Box,
@@ -11,6 +13,16 @@ import {
 } from "@mui/material";
 import UnfoldMoreIcon from "@mui/icons-material/UnfoldMore";
 import { ArrowBack, UnfoldLess } from "@mui/icons-material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { pink } from "@mui/material/colors";
+import "../style.css";
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#000",
+    },
+  },
+});
 
 function NavBar({
   TotalCount,
@@ -22,57 +34,65 @@ function NavBar({
   Context,
 }) {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography
-          variant="h6"
-          noWrap
-          component="div"
-          sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-        >
-          {Context === "vendor_view" && (
-            <>
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="open drawer"
-                sx={{ mr: 2 }}
-              >
-                <ArrowBack />
-              </IconButton>
-              <Chip
-                label={`#${OrderID} - ${OrderDate}`}
-                variant="outlined"
-                sx={{ mr: 5 }}
-              />
-            </>
-          )}
-          <Chip label={`Total Messages ${TotalCount}`} variant="outlined" />
-        </Typography>
-        <Box sx={{ display: { xs: "none", md: "flex" } }}>
-          <TextField
-            label="Search"
-            color="primary"
-            size="small"
-            variant="outlined"
-            id="margin-none"
-            onChange={(e) => onSearchThread(e.target.value)}
-          />
-
-          <IconButton
-            onClick={() => onCollapsed(!showMore)}
-            size="small"
-            edge="end"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ ml: 3 }}
+    <ThemeProvider theme={darkTheme}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", sm: "block" },
+            }}
           >
-            {showMore ? <UnfoldLess /> : <UnfoldMoreIcon />}
-          </IconButton>
-        </Box>
-      </Toolbar>
-    </AppBar>
+            {Context === "vendor_view" && (
+              <>
+                <IconButton
+                  size="large"
+                  edge="start"
+                  aria-label="open drawer"
+                  sx={{ mr: 2 }}
+                >
+                  <ArrowBack />
+                </IconButton>
+                <Chip
+                  label={`#${OrderID} - ${OrderDate}`}
+                  variant="outlined"
+                  sx={{ mr: 5, color: "#fff" }}
+                />
+              </>
+            )}
+            <Chip
+              label={`Total Messages ${TotalCount}`}
+              variant="outlined"
+              sx={{ color: "white" }}
+            />
+          </Typography>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <TextField
+              label="Search"
+              size="small"
+              color="info"
+              variant="standard"
+              id="margin-none"
+              onChange={(e) => onSearchThread(e.target.value)}
+            />
+
+            <IconButton
+              onClick={() => onCollapsed(!showMore)}
+              size="small"
+              edge="end"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ ml: 3 }}
+            >
+              {showMore ? <UnfoldLess /> : <UnfoldMoreIcon />}
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   );
 }
 
