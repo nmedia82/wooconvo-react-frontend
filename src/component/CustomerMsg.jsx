c280e4ae02b145e96ace6e1db77f9192da408a77
WPBASILAHMAD <basilmedia7@gmail.com>
2022-12-19 10:20:17 +0500

    Create file Upload Icon

--- src/component/CustomerMsg.jsx
+++ src/component/CustomerMsg.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import { useEffect, useState, useRef } from "react";
 import {
   Typography,
   Grid,
@@ -10,19 +11,23 @@ import {
   Divider,
   IconButton,
   Tooltip,
+  Button,
+  Box,
 } from "@mui/material";
 import { blue, green } from "@mui/material/colors";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import CloudUploadOutlinedIcon from "@mui/icons-material/CloudUploadOutlined";
 
-export default function CustomerMsg({ message, showMore }) {
+export default function CustomerMsg({ props, message, showMore }) {
   function stringAvatar(name) {
     return {
       children: `${name.split(" ")[0][0]}`,
     };
   }
   const [open, setOpen] = useState(false);
+  const [file, setFile] = useState(null);
+  const hiddenFileInput = useRef(null);
 
   useEffect(() => {
     setOpen(showMore);
@@ -30,6 +35,9 @@ export default function CustomerMsg({ message, showMore }) {
 
   const handleClick = () => {
     setOpen(!open);
+  };
+  const handleclick = (event) => {
+    hiddenFileInput.current.click();
   };
 
   return (
@@ -73,13 +81,51 @@ export default function CustomerMsg({ message, showMore }) {
           </Typography>
           <Tooltip title="Upload File">
             <IconButton
-              // onClick={handleFileUpload}
               aria-label=""
-              sx={{ mt: 2, backgroundColor: blue[800] }}
+              sx={{ mb: 2, backgroundColor: blue[800] }}
             >
               <CloudUploadOutlinedIcon sx={{ color: "white" }} />
             </IconButton>
           </Tooltip>
+          <Box
+            sx={{
+              flexDirection: "row",
+              display: "flex",
+            }}
+          >
+            <Box className="preview-thumb-upload">
+              <img
+                src="https://najeebmedia.com/wp-content/uploads/2021/07/cropped-n-media-logo-1-300x300.png"
+                className="preview-thumb-img-upload"
+                height="50"
+                width="100"
+                // id={`preview-${file.name}`}
+                // alt={file.name}
+              />
+              <p className="preview-thumb-tool-upload">
+                <Typography variant="span" display={"inline"}>
+                  {/* {file.name} */}
+                  N-Media
+                </Typography>
+              </p>
+            </Box>
+            <Box className="preview-thumb-upload">
+              <img
+                src="https://new.najeebmedia.com/wp-content/uploads/2022/05/1.png"
+                className="preview-thumb-img-upload"
+                height="50"
+                width="100"
+                // id={`preview-${file.name}`}
+                // alt={file.name}
+              />
+              <p className="preview-thumb-tool-upload">
+                <Typography variant="span" display={"inline"}>
+                  {/* {file.name} */}
+                  Google Sync
+                </Typography>
+              </p>
+            </Box>
+          </Box>
         </ListItemText>
       </Collapse>
     </div>
